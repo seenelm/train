ae4718c8e7bc80da90bbc86a85df8c51fede2f17
Yassine Elmellouki <yelmellouki@gmail.com>
2023-09-28 21:15:11 -0400

    testig

--- src/features/auth/loginUser.js
+++ src/features/auth/loginUser.js
@@ -12,7 +12,11 @@ export const loginUser = createAsyncThunk(
         password: password,
       };
 
-      const response = await axios.post(`${Config.API_URL}/api/login`, data);
+      //const response = await axios.post(`${Config.API_URL}/api/login`, data);
+      const response = await axios.post(
+        "http://192.168.1.24:3000/api/login",
+        data
+      );
 
       await Keychain.setGenericPassword(
         response.data.userId,
