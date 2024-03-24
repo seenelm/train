3edb202f94c6ea8cbe11ceffcee238d790f1a255
Yassine Elmellouki <yelmellouki@gmail.com>
2024-03-24 19:07:29 -0400

    deleting grouptype and refactoring later

--- src/features/groups/groupType.ts
+++
@@ -1,7 +0,0 @@
-// Define what properties a Group has based on your application's needs
-interface GroupType {
-    id: string; // Assuming every group has an ID
-    name: string; // The name of the group
-    // Add any other properties that a group might have
-  }
-  
